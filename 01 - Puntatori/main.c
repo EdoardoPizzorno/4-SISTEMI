631881e5dc1cfbbb3dacd3571674ef7c1a79f69f
Edoardo Pizzorno <e.pizzorno.2293@vallauri.edu>
2022-09-22 08:04:03 +0200

    Codeblocks sul PC della scuola sistemato
    Upload di tutti i file che non erano stati caricati dato che c'erano problemi con codeblocks

---
+++ 01 - Puntatori/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n=8;
+    int *p;
+
+    printf("Size(int): %d\nSize(float): %d\nSize(char): %d",sizeof(int), sizeof(char), sizeof(float));
+
+    printf("Size(int*): %d\nSize(float*): %d\nSize(char*): %d",sizeof(int*), sizeof(char*), sizeof(float*));
+
+    //Leggo contenuto/indirizzo n
+    printf("Contenuto n: %d - Indirizzo di n: %p\n", n, &n);
+
+    //Leggo contenuto/indirizzo *p
+    printf("Contenuto p: %d - Indirizzo di p: %p\n", p, &p);
+
+    p=&n; //in p metto l'indirizzo di n
+    printf("Indirizzo p dopo assegnazione a n: %p", p);
+
+    //stampo contenuto di n
+    printf("\nContenuto di n: (classico)%d - (puntatore)%d", n, *p); //con *p mi stampa 8 perchè c'è il contenuto di n
+    *p = 5;
+    printf("\nContenuto di n: (classico)%d - (puntatore)%d", n, *p); //ora entrambe diventano 8 perchè sto lavorando sulla stessa cella
+    return 0;
+}

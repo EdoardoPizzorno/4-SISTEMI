6f900613d369d9b917944baa9ff3400581146adc
Edoardo Pizzorno <e.pizzorno.2293@vallauri.edu>
2022-10-05 20:23:24 +0200

    Compiti stringhe2

--- 06 - Compiti stringhe2/main.c
+++ 06 - Compiti stringhe2/main.c
@@ -8,15 +8,16 @@ int menu(){
     int sc=0;
 
     printf("0. ESCI\n");
-    printf("1. Cifratura\n");
+    printf("1. Cifratura semplice\n");
+    printf("2. Cifratura di Cesare\n");
 
     printf("Scelta --> ");
     scanf("%d",&sc);
 
     return sc;
 }
-void cifraturaCesare(char *s, int n);
-void cifratura2(char *s, int n);
+void cifraturaSemplice(char *s, int n);
+void cifraturaCesare(char *s, char *key);
 
 int main()
 {
@@ -25,6 +26,8 @@ int main()
     char s[BUFFER];
 
     int n=0;
+
+    char key=' ';
 
     do{
         fflush(stdin);
@@ -38,16 +41,19 @@ int main()
             gets(s);
             printf("Inserisci n: ");
             scanf("%d",&n);
-            cifraturaCesare(&s,n);
+            cifraturaSemplice(&s,n);
             printf("\nStringa modificata: %s",s);
             break;
         case 2:
             getchar();
             printf("Inserisci stringa: ");
             gets(s);
-            printf("Inserisci n: ");
-            scanf("%d",&n);
-            cifratura2(&s, n);
+            do{
+                printf("\nInserisci chiave: ");
+                scanf("%c",&key);
+            }while(key<=64 || key>=91 && key<=96 || key>=123);
+            cifraturaCesare(&s, &key);
+            printf("\nStringa modificata: %s",s);
         }
         fflush(stdin);
         getchar();
@@ -56,11 +62,32 @@ int main()
     printf("Programma terminato...");
     return 0;
 }
-void cifratura2(char *s, int n){
-}
-void cifraturaCesare(char *s, int n){
+void cifraturaCesare(char *s, char *key){
+    if(*key>='a' && *key<='z')
+        *key-=('a'-1);
+    else if(*key>='A' && *key<='Z')
+        *key-=('A'-1);
+    printf("%d",*key);
     while(*s != '\0'){
-        *s+=n;
+        if(*s >= 'a' && *s <= 'z' || *s >= 'A' && *s <= 'Z'){
+            if(*s >= ('z'-(*key-1)) && *s <= 'z')
+                *s -= (23+*key+1);
+            else *s += *key;
+        }
+        if(*s >= 'A' && *s <= 'Z'){
+            if(*s >= ('Z'-(*key-1)) && *s <= 'Z')
+                *s -= (23+*key+1);
+            else *s += *key;
+         }
+        s++;
+    }
+}
+void cifraturaSemplice(char *s, int n){
+    while(*s != '\0'){
+        if(*s+n > 64 && *s+n < 91)
+            *s+=n;
+        if(*s+n > 96 && *s+n<123)
+            *s+=n;
         s++;
     }
 }
